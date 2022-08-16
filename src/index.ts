5841b18506202a039b6343bf7db9d33330959352
philios33 <phil@code67.com>
2022-08-16 23:27:27 +0200

    Rejigging the exports

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
-import { TopicProducer } from "./topicProducer";
-import { TopicConsumer } from "./topicConsumer";
-import TopicSetter from "./topicSetter";
+import { TopicProducer as TopicProducerAlias } from "./topicProducer";
+import { TopicConsumer as TopicConsumerAlias } from "./topicConsumer";
 
-import { MongoRedisTopicConsumer } from './backends/mongoRedis/consumer';
-import { MongoRedisTopicProducer } from './backends/mongoRedis/producer';
+import TopicSetterAlias from "./topicSetter";
+
+import { MongoRedisTopicConsumer as MongoConsumerAlias } from './backends/mongoRedis/consumer';
+import { MongoRedisTopicProducer as MongoProducerAlias } from './backends/mongoRedis/producer';
 
 export default {
-    TopicProducer,
-    TopicConsumer,
+    TopicProducer: TopicProducerAlias,
+    TopicConsumer: TopicConsumerAlias,
     
-    TopicSetter,
+    TopicSetter: TopicSetterAlias,
 
     // Bonus backends already implemented for you
-    MongoRedisTopicConsumer,
-    MongoRedisTopicProducer,
+    MongoRedisTopicConsumer: MongoConsumerAlias,
+    MongoRedisTopicProducer: MongoProducerAlias,
 }
+
+export const TopicProducer = TopicProducerAlias;
+export const TopicConsumer = TopicConsumerAlias;
+
+export const TopicSetter = TopicSetterAlias;
+
+export const MongoRedisTopicConsumer = MongoConsumerAlias;
+export const MongoRedisTopicProducer = MongoProducerAlias;
